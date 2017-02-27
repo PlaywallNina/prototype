c9329b5d590372cd12c7f766468cefa77f23a01a
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-02-27 17:11:40 +0100

    add admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'static#home'
-  get 'static/home'
-
   devise_for :admins
+
+  namespace :admin do
+    resources :surveys do
+      resources :questions do
+        resources :answers
+      end
+    end
+  end
 end
