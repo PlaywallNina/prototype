1be799a1426badf5e701aa7ee81017bca9fecfec
Thanosky <kthanoss90@gmail.com>
2017-02-28 14:46:44 +0100

    finally the right files

---
+++ app/javascript/React/components/Brand.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Brand extends Component {
+  render() {
+    return(
+      <div>
+        <img
+          src={'https://s3.eu-central-1.amazonaws.com/theplaywall/The_Playwall_Logo.png'},
+        ></img>
+      </div>
+    )
+  }
+}
+
+export default Brand
