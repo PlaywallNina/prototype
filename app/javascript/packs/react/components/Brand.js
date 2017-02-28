f81cfd68702f113c9e58a11c074deee09535f444
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-02-28 11:26:53 +0100

    logo to aws

--- app/javascript/packs/react/components/Brand.js
+++ app/javascript/packs/react/components/Brand.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import Logo from '../images/The_Playwall_Logo.png'
 
 export class Brand extends Component {
   render() {
     return(
       <div>
-        <img src={Logo}></img>
+        <img
+          src='https://s3.eu-central-1.amazonaws.com/theplaywall/The_Playwall_Logo.png?X-Amz-Date=20170228T102251Z&X-Amz-Expires=300&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Signature=21618ccf6cc2cf4422696ed78ee245c857605cbdc0f1f74480cd8004b56a006e&X-Amz-Credential=ASIAIGG22ITFSTEWGTZA/20170228/eu-central-1/s3/aws4_request&X-Amz-SignedHeaders=Host&x-amz-security-token=FQoDYXdzEFsaDPrDRsi4jU1nyNJOUCL6AW15NEbx0Og4j9pcZEcb1RNKlw86lGGiccsDUv3iG9vbpYRZ2SV1LbFnlXdCLbaRXi7O1MLr%2BahAo0lRNCgXo5xzEepHItWdA/ehvGBRyWOxsXwP/Fbw%2B6z6efGCF2hAzK6AdG0qSNOvxc0ddgWDWhZHzwsB0X0wDxja5LJCDS0DKlQhHWwir0bpQDHXV9xTKNU5FYD/yfLxGciId/fkG%2Bj8pFLYYvZkj3DykBzkDQVuppLeOQevqkx0OTfrNqzkIz2KG0lwYcaobAkVG1Y9GIf8aJ%2Bx17rs9CFq9voa0N1jv9M1INe9R%2Bm6qUjJ0r/AVDkgfcEdxGxm6YMo8Z3VxQU%3D',
+          width: 300,
+          height: 300></img>
       </div>
     )
   }
