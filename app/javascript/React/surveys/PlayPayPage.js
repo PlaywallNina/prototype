5b0425eb528b9621ea157ada9a3d57c71ac5cbaf
Robert Jan Hogerbrugge <rj.hogerbrugge@hotmail.com>
2017-03-01 13:34:35 +0100

    styling

--- app/javascript/React/surveys/PlayPayPage.js
+++ app/javascript/React/surveys/PlayPayPage.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
-import './PlayPayPage.sass'
+import './PlayPayPage.scss'
 import { history } from '../store'
 import {browserHistory} from 'react-router';
 
@@ -26,16 +26,19 @@ export class PlayPayPage extends React.Component {
 
     return(
 
-        <article className="playpay">
+
+      <article className="playpay">
+        <div className="info-exit">
+          <p><button className="info" onClick={ this.props.closeModal }>i</button></p>
+          <p><button className="exit" onClick={ this.props.closeModal }>x</button></p>
+        </div>
           <h1>De limiet van 5 gratis</h1>
           <h1>artikelen is</h1>
           <h1>bereikt.</h1>
           <p>Dit artikel kost € 0,75. Betaal via iDeal  </p>
           <p>of krijg het artikel gratis via The Playwall  </p>
-          <p><button onClick={ this.props.closeModal }>i</button></p>
-          <p><button onClick={this.payClick.bind(this)}>Betaal met iDeal</button></p>
-          <p><button onClick={this.playClick.bind(this)}>Play to pay</button></p>
-          <p><button onClick={ this.props.closeModal }>x</button></p>
+          <p><button className="pay" onClick={ this.payClick.bind(this) }>Betaal met iDeal</button></p>
+          <p><button className="play" onClick={this.playClick.bind(this) }>Play to pay</button></p>
         </article>
 
     )
