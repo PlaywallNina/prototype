86dcd6fa0034b9d981171f10c20007e95475978a
StefanKollaart <garbera@icloud.com>
2017-03-01 12:18:50 +0100

    Can make API request to save new answer - not secure yet and not yet with session id

--- app/controllers/given_answers_controller.rb
+++ app/controllers/given_answers_controller.rb
@@ -1,2 +1,24 @@
 class GivenAnswersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    given_answer = GivenAnswer.new(given_answer_params)
+
+    if given_answer.save
+      render status: 201, json: {
+        message: "Answer successfully saved",
+        given_answer: given_answer
+      }.to_json
+    else
+      render status: 422, json: {
+        errors: given_answer.errors
+      }.to_json
+    end
+  end
+
+  private
+
+  def given_answer_params
+    params.require(:given_answer).permit(:session_id, :answer_id)
+  end
 end
