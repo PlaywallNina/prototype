c21055989356fd410827afc9452fb88583d1018d
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-01 13:24:23 +0100

    Allow answers to be saved in the database

--- app/javascript/React/App.js
+++ app/javascript/React/App.js
@@ -2,29 +2,19 @@ import './App.sass'
 import React from 'react'
 import { connect } from 'react-redux'
 import getQuestions from './actions/questions/fetch'
-import sendAnswer from './actions/questions/send-answer'
 
 class App extends React.Component {
   componentDidMount() {
     this.props.getQuestions()
   }
 
-  sendTest(e) {
-    e.preventDefault()
-    sendAnswer(this.props.currentQuestion.answers[0].id)
-  }
   render() {
     return (
       <div className='content'>
-        <button onClick={this.sendTest.bind(this)}>Test 1</button>
           { this.props.children }
       </div>
     )
   }
 }
 
-const mapStateToProps = ({questions}) => ({
-  questions,
-  currentQuestion: questions[0]
-})
-export default connect(mapStateToProps, { getQuestions })(App)
+export default connect(null, { getQuestions })(App)
--- app/javascript/React/reducers/questions.js
+++ app/javascript/React/reducers/questions.js
@@ -1,13 +1,17 @@
 import {
   GET_QUESTIONS,
   GET_QUESTIONS_ERROR,
-  GET_QUESTIONS_RECEIVED
+  GET_QUESTIONS_RECEIVED,
+  POST_ANSWER_SUCCESS
 } from '../middleware/api'
 
 export default(state = [], { type, payload }) => {
   switch(type) {
     case GET_QUESTIONS_RECEIVED:
       return [].concat(payload)
+
+    case POST_ANSWER_SUCCESS:
+      return state
 
     default:
       return state
