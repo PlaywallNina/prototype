652127b788552d0f91b17d874bb3c2e3078a8aab
Thanosky <kthanoss90@gmail.com>
2017-03-01 13:18:56 +0100

    turnd buttons to divs and added background and hover

--- app/javascript/React/surveys/SurveyPage.js
+++ app/javascript/React/surveys/SurveyPage.js
@@ -12,10 +12,10 @@ export class SurveyPage extends PureComponent {
           <h2>The Question!</h2>
         </div>
         <div className="awrapper">
-          <button className="but" id="a1">Answer 1</button>
-          <button className="but" id="a2">Answer 2</button>
-          <button className="but" id="a3">Answer 3</button>
-          <button className="but" id="a4">Answer 4</button>
+          <div className="but" id="a1">Answer 1</div>
+          <div className="but" id="a2">Answer 2</div>
+          <div className="but" id="a3">Answer 3</div>
+          <div className="but" id="a4">Answer 4</div>
         </div>
       </div>
     )
