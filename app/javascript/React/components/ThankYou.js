85817ded55b31673985180094dcd8698c2beb6c0
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-02 13:56:36 +0100

    move thankyou css

--- app/javascript/React/components/ThankYou.js
+++ app/javascript/React/components/ThankYou.js
@@ -1,16 +1,14 @@
 import React, { PureComponent } from 'react'
-import './ThankYou.scss'
 
 export class ThankYou extends PureComponent {
-
   render() {
     window.setTimeout(function(){
       window.location.href = "https://www.svdj.nl/projects/the-playwall/";
     }, 3000);
 
     return(
-      <div className="modal-container">
-        <div className='thanks-text'>
+      <div className="main-container">
+        <div className='main-text'>
           <h1>Thanks!</h1>
 
           <p>Je hebt nu toegang tot je gratis artikel.</p>
