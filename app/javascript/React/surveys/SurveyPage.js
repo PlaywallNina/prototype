5a6fd654e0db3c7faec79620f01641da283fb0c0
Stefan Kollaart <garbera@icloud.com>
2017-03-02 14:13:11 +0100

    Update SurveyPage.js
    Changed back to images from database because of changed seeds

--- app/javascript/React/surveys/SurveyPage.js
+++ app/javascript/React/surveys/SurveyPage.js
@@ -42,25 +42,25 @@ export class SurveyPage extends PureComponent {
             id="a1"
             onClick={this.handleClick.bind(this, this.props.currentQuestion.answers[0].id)}
             >
-            <img src='https://placeholdit.imgix.net/~text?txtsize=9&txt=150%C3%97150&w=150&h=150'></img>
+-            <img src={this.props.currentQuestion.answers[0].picture}></img>
           </div>
           <div className="but"
             id="a2"
             onClick={this.handleClick.bind(this, this.props.currentQuestion.answers[1].id)}
             >
-            <img src='https://placeholdit.imgix.net/~text?txtsize=9&txt=150%C3%97150&w=150&h=150'></img>
+-            <img src={this.props.currentQuestion.answers[1].picture}></img>
           </div>
           <div className="but"
             id="a3"
             onClick={this.handleClick.bind(this, this.props.currentQuestion.answers[2].id)}
             >
-            <img src='https://placeholdit.imgix.net/~text?txtsize=9&txt=150%C3%97150&w=150&h=150'></img>
+-            <img src={this.props.currentQuestion.answers[2].picture}></img>
           </div>
           <div className="but"
             id="a4"
             onClick={this.handleClick.bind(this, this.props.currentQuestion.answers[3].id)}
             >
-            <img src='https://placeholdit.imgix.net/~text?txtsize=9&txt=150%C3%97150&w=150&h=150'></img>
+-            <img src={this.props.currentQuestion.answers[3].picture}></img>
           </div>
         </div>
         <footer>{this.props.questionIndex + 1}/{this.props.questions.length}</footer>
