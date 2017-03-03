faa0d78de2c2fa5b34415b2ab4a4f84cb35e5d1b
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-03 14:18:25 +0100

    fix survey count bug

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,9 @@
 class QuestionsController < ApplicationController
   def index
-    @survey = Survey.find(rand(Survey.first.id..(Survey.first.id + Survey.all.length - 1)))
-    
+    unless Survey.count == 0
+      @survey = Survey.find(rand(Survey.first.id..(Survey.first.id + Survey.all.length - 1)))
+    end
+
     if @survey != nil && (@survey.questions != nil || @survey.questions = 5)
       @questions = @survey.questions
     else
