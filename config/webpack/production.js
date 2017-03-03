bfa5467185da60321a7be7b9b5e0878cf965847e
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-03-03 12:30:01 +0100

    add sass loader

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -7,7 +7,22 @@ const sharedConfig = require('./shared.js')
 
 module.exports = merge(sharedConfig.config, {
   output: { filename: '[name]-[chunkhash].js' },
-
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+      }, {
+          loader: "css-loader" // translates CSS into CommonJS
+      }, {
+          loader: "sass-loader" // compiles Sass to CSS
+      }]
+    },{
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      loader: 'url?limit=10000!img?progressive=true'
+      }
+    ]
+  },
   plugins: [
     new webpack.LoaderOptionsPlugin({
       minimize: true
